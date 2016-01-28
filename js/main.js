0bef6cdaa1dbbb9ec3e63ebc1cf331a11e780d1d
Patrik Wall <patrik.wall@thirdbase.se>
2016-01-28 14:38:19 +0100

    Added time

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
-    
-    //Load stryktips
+
+    var dummyViewModel = function() {
+        var self = this;
+
+        self.currentBetter = ko.observable("Patrik Wall");
+        self.currentDate = ko.observable(moment(new Date()).format('ll'));
+        self.currentTime = ko.observable(moment(new Date()).format('HH:mm:ss'));
+
+        var updateDateString = function () {
+            self.currentDate(moment(new Date()).format('ll'));
+            self.currentTime(moment(new Date()).format('HH:mm:ss'));
+        };
+
+        self.init = function () {
+            console.log("init");
+            setInterval(updateDateString, 1000);
+
+        };
+
+        self.init();
+    };
+    ko.applyBindings(new dummyViewModel());
 });
